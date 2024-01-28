c7a0246a89a158edb1d9391a2450e6dabca6d361
Dgebe Nicolas <64973322+dgebenicolas@users.noreply.github.com>
2024-01-29 08:07:37 +0900

    HW 1 question 3.sql

--- Homework1/HW 1 question 3.sql
+++ Homework1/HW 1 question 3.sql
@@ -1,6 +1,6 @@
 SELECT 
  CAST(lpep_pickup_datetime AS DATE) as "day",
  COUNT(1)
- FROM green_taxi_trips
+FROM green_taxi_trips
 WHERE CAST(lpep_pickup_datetime AS DATE) = '2019-09-18'
 GROUP BY CAST(lpep_pickup_datetime AS DATE)
